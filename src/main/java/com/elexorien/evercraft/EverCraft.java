c5d272f26c63637ef0f0910009e3531e62a172b3
Elexorien <atharsia@yahoo.com>
2014-06-21 07:45:58 -0500

    Testing commits

---
+++ src/main/java/com/elexorien/evercraft/EverCraft.java
@@ -0,0 +1,29 @@
+package com.elexorien.evercraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="EverCraft", name="EverCraft", version="1.7.2-0.1")
+public class EverCraft
+{
+    @Mod.Instance("EverCraft")
+    public static EverCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
